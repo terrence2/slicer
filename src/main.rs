32141fce88f70f0f3ea8c68b98195cf9bbe19b0c
Terrence Cole <terrence.cole@procore.com>
2017-05-10 20:00:31 -0700

    Add initial cli parser.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[macro_use] extern crate approx;
+#[macro_use] extern crate clap;
 #[macro_use] extern crate error_chain;
 #[macro_use] extern crate nom;
 extern crate nalgebra;
@@ -14,8 +15,19 @@ use errors::{Result, ResultExt};
 
 quick_main!(run);
 fn run() -> Result<()> {
-    let mut fp = File::open("test_data/cube_bin.stl").unwrap();
-    let stl = StlMesh::from_file(&mut fp).unwrap();
+    let parser = clap_app!(slicer =>
+        (version: "0.1.0")
+        (author: "Terrence Cole <terrence.d.cole@gmail.com>")
+        (about: "Slice multi-color models smarter.")
+        (@arg INPUT: +required ... "Sets the input file(s) to use.")
+    );
+    let matches = parser.get_matches();
+
+    let filenames = matches.values_of("INPUT").unwrap().collect::<Vec<&str>>();
+    for filename in filenames {
+        let mut fp = File::open(filename).chain_err(|| "failed to open source files")?;
+        let stl = StlMesh::from_file(&mut fp).chain_err(|| "failed to load stl file")?;
+    }
 
     //let foo = m.intersect_plane();
 
